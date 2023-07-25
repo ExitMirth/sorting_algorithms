36b9ad6e1b0e47a165d8da79c2f7e36aa4a90fed
Reliccode <bryanmaritim@gmail.com>
2023-07-25 14:37:55 +0000

    feat: Implement Insertion sort algorithm for doubly linked list

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,42 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Sorts doubly linked list of ints
+ * in ascending order using Insertion sort algorithm.
+ * @list: Double pointer to head of doubly linked list.
+ */
+void insertion_sort_list(listint_t **list)
+{
+	if (!list || !*list || !(*list)->next)
+		return;
+
+	listint_t *current, *key, *prev;
+
+	current = (*list)->next;
+
+	while (current)
+	{
+		key = current;
+		prev = current->prev;
+
+		while (prev && prev->n > key->n)
+		{
+			/* Swap nodes */
+			if (key->next)
+				key->next->prev = prev;
+			prev->next = key->next;
+			key->prev = prev->prev;
+			key->next = prev;
+			prev->prev = key;
+
+			if (key->prev)
+				key->prev->next = key;
+			else
+				*list = key;
+
+			print_list(*list);
+			prev = key->prev;
+		}
+		current = current->next;
+	}
+}
