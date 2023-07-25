946435a28a1fc04b14e7583324addd8b9957ae56
Reliccode <bryanmaritim@gmail.com>
2023-07-25 15:02:57 +0000

    fix: Implemented fix for logic error that caused incorrect sorting by traversing list and swapping nodes

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,42 +1,34 @@
 #include "sort.h"
-
 /**
- * insertion_sort_list - Sorts doubly linked list of ints
- * in ascending order using Insertion sort algorithm.
- * @list: Double pointer to head of doubly linked list.
- */
+* insertion_sort_list - Sorts a doubly linked list of ints
+* in ascending order using insertion sorting algorithm
+* @list: Double pointer to head of doubly linked list.
+*/
 void insertion_sort_list(listint_t **list)
 {
-	if (!list || !*list || !(*list)->next)
+	listint_t *tmp;
+	int n;
+
+	if (!list)
 		return;
-
-	listint_t *current, *key, *prev;
-
-	current = (*list)->next;
-
-	while (current)
+	tmp = *list;
+	while (tmp)
 	{
-		key = current;
-		prev = current->prev;
-
-		while (prev && prev->n > key->n)
+		while (tmp)
 		{
-			/* Swap nodes */
-			if (key->next)
-				key->next->prev = prev;
-			prev->next = key->next;
-			key->prev = prev->prev;
-			key->next = prev;
-			prev->prev = key;
-
-			if (key->prev)
-				key->prev->next = key;
-			else
-				*list = key;
-
-			print_list(*list);
-			prev = key->prev;
+			if (tmp->next)
+			{
+				if (tmp->n > tmp->next->n)
+				{
+					n = tmp->n;
+					*(int *)&tmp->n = tmp->next->n;
+					*(int *)&tmp->next->n = n;
+					tmp = *list;
+					print_list(*list);
+					break;
+				}
+			}
+			tmp = tmp->next;
 		}
-		current = current->next;
 	}
 }
