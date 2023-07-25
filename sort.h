426618da0e6788de911fc63e264f65a8759fdbca
Reliccode <bryanmaritim@gmail.com>
2023-07-25 14:09:25 +0000

    feat: Add bubble_sort function prototype to sort.h

--- sort.h
+++ sort.h
@@ -8,20 +8,20 @@
  *
  * @n: Int stored in node
  * @prev: Pointer to prev element of list
- * @next: Pointer to the next element of the list
+ * @next: Pointer to next element of list
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 /* print funcs prototypes */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
-/* Bubble sort */
+/* bubble sort */
 void bubble_sort(int *array, size_t size);
 
 #endif /* SORT_H */
