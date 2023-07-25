576801190b8d203b3e4568d2829e0575a524b50f
Reliccode <bryanmaritim@gmail.com>
2023-07-25 18:29:08 +0000

    fix: Correcting logic for swapping nodes in the doubly linked list during sorting process

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,42 +1,42 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - Sorts a doubly linked list of ints
- *                       in ascending order using Insertion sort algorithm.
+ * insertion_sort_list - Sorts doubly linked list of ints in ascending order
+ *                        using Insertion sort algorithm.
+ *
  * @list: Double pointer to head of doubly linked list.
  */
 void insertion_sort_list(listint_t **list)
 {
-    if (!list || !*list || !(*list)->next)
+    listint_t *curr, *tmp;
+
+    if (list == NULL || *list == NULL || (*list)->next == NULL)
         return;
 
-    listint_t *current, *key, *prev;
+    curr = (*list)->next;
 
-    current = (*list)->next;
-
-    while (current)
+    while (curr != NULL)
     {
-        key = current;
-        prev = current->prev;
-
-        while (prev && prev->n > key->n)
+        tmp = curr->prev;
+        while (tmp != NULL && tmp->n > curr->n)
         {
-            /* Swap nodes */
-            if (key->next)
-                key->next->prev = prev;
-            prev->next = key->next;
-            key->prev = prev->prev;
-            key->next = prev;
-            prev->prev = key;
-
-            if (key->prev)
-                key->prev->next = key;
+            if (tmp->prev != NULL)
+                tmp->prev->next = curr;
             else
-                *list = key;
+                *list = curr;
+
+            if (curr->next != NULL)
+                curr->next->prev = tmp;
+
+            tmp->next = curr->next;
+            curr->prev = tmp->prev;
+            curr->next = tmp;
+            tmp->prev = curr;
 
             print_list(*list);
-            prev = key->prev;
+
+            tmp = curr->prev;
         }
-        current = current->next;
+        curr = curr->next;
     }
 }
--- sort.h
+++ sort.h
@@ -1,14 +1,13 @@
-#ifndef SORT_H
-#define SORT_H
-
+#ifndef _SORT_H_
+#define _SORT_H_
 #include <stddef.h>
 
 /**
  * struct listint_s - Doubly linked list node
  *
- * @n: Int stored in node
- * @prev: Pointer to prev element of list
- * @next: Pointer to next element of list
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
  */
 typedef struct listint_s
 {
@@ -17,11 +16,21 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/* print funcs prototypes */
-void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
-/* bubble sort */
+void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void counting_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
-#endif /* SORT_H */
+
+#endif
