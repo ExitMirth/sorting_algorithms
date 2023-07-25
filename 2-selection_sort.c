1350f5a4304adcfae28a6ce7526ccf3c02d47704
Reliccode <bryanmaritim@gmail.com>
2023-07-25 19:04:55 +0000

    fix: Adjusting code indentation

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,22 +1,21 @@
 #include "sort.h"
 
 /**
- * selection_sort - Sorts array of ints in ascending order
- *                  using Selection sort algorithm.
- * @array: array to be sorted.
- * @size: Num of elements in array.
+ * selection_sort - Sorts array of integers in ascending order using
+ * Selection sort algorithm.
+ *
+ * @array: Pointer to array to be sorted.
+ * @size: Size of array.
  */
 void selection_sort(int *array, size_t size)
 {
-	if (!array || size < 2)
-		return;
-
 	size_t i, j, min_idx;
-	int temp;
+	int tmp;
 
 	for (i = 0; i < size - 1; i++)
 	{
 		min_idx = i;
+
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_idx])
@@ -25,9 +24,10 @@ void selection_sort(int *array, size_t size)
 
 		if (min_idx != i)
 		{
-			temp = array[i];
+			tmp = array[i];
 			array[i] = array[min_idx];
-			array[min_idx] = temp;
+			array[min_idx] = tmp;
+
 			print_array(array, size);
 		}
 	}
