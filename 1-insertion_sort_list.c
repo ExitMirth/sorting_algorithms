a13e843fd7c3d6314d2a96e1ee2fa841b59f343c
Reliccode <bryanmaritim@gmail.com>
2023-07-25 21:25:07 +0000

    [Refactor]: fix: Implemented fix for logic error that caused incorrect sorting by… … traversing list and swapping nodes

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,42 +1,33 @@
 #include "sort.h"
-
 /**
- * insertion_sort_list - Sorts doubly linked list of ints in ascending order
- *                        using Insertion sort algorithm.
- *
- * @list: Double pointer to head of doubly linked list.
- */
+* insertion_sort_list - insertion sorting algorithm
+* @list: linked list to sort
+*/
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *curr, *tmp;
+	listint_t *tmp;
+	int n;
 
-    if (list == NULL || *list == NULL || (*list)->next == NULL)
-        return;
-
-    curr = (*list)->next;
-
-    while (curr != NULL)
-    {
-        tmp = curr->prev;
-        while (tmp != NULL && tmp->n > curr->n)
-        {
-            if (tmp->prev != NULL)
-                tmp->prev->next = curr;
-            else
-                *list = curr;
-
-            if (curr->next != NULL)
-                curr->next->prev = tmp;
-
-            tmp->next = curr->next;
-            curr->prev = tmp->prev;
-            curr->next = tmp;
-            tmp->prev = curr;
-
-            print_list(*list);
-
-            tmp = curr->prev;
-        }
-        curr = curr->next;
-    }
+	if (!list)
+		return;
+	tmp = *list;
+	while (tmp)
+	{
+		while (tmp)
+		{
+			if (tmp->next)
+			{
+				if (tmp->n > tmp->next->n)
+				{
+					n = tmp->n;
+					*(int *)&tmp->n = tmp->next->n;
+					*(int *)&tmp->next->n = n;
+					tmp = *list;
+					print_list(*list);
+					break;
+				}
+			}
+			tmp = tmp->next;
+		}
+	}
 }
