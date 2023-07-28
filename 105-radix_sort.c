d3b0875bcbcd51a94e436a9680d7cb28ece94148
Reliccode <bryanmaritim@gmail.com>
2023-07-28 14:00:34 +0000

    fix: Calculated max value in array to determine num of iterations needed from the num of digits in max value

--- 105-radix_sort.c
+++ 105-radix_sort.c
@@ -3,76 +3,44 @@
 #include "sort.h"
 
 /**
- * getMax - Helper func to get max value in array
- * @array: Pointer to array
- * @size: array size
- * Return: max value in array
- */
-int getMax(int *array, size_t size)
-{
-	int max = array[0];
-	size_t i;
-
-	for (i = 1; i < size; i++)
-	{
-		if (array[i] > max)
-		{
-			max = array[i];
-		}
-	}
-
-	return (max);
-}
-
-/**
- * countingSort - Helper func to do counting sort on array
- * @array: Pointer to array
- * @size: array size
- * @exp: current digit to sort by
- */
-void countingSort(int *array, size_t size, int exp)
-{
-	int *output = malloc(sizeof(int) * size);
-	int count[10] = {0};
-	size_t i;
-
-	for (i = 0; i < size; i++)
-	{
-		count[(array[i] / exp) % 10]++;
-	}
-
-	for (i = 1; i < 10; i++)
-	{
-		count[i] += count[i - 1];
-	}
-
-	for (i = size; i > 0; i--)
-	{
-		output[count[(array[i - 1] / exp) % 10] - 1] = array[i - 1];
-		count[(array[i - 1] / exp) % 10]--;
-	}
-
-	for (i = 0; i < size; i++)
-	{
-		array[i] = output[i];
-	}
-
-	free(output);
-}
-
-/**
- * radix_sort - algorithm to sort array of ints in ascending order
- * @array: Pointer to array
- * @size: Size of array
+ * radix_sort - Sorts array of ints in ascending order using Radix sort.
+ * @array: Array to be sorted.
+ * @size: Size of array.
  */
 void radix_sort(int *array, size_t size)
 {
-	int max = getMax(array, size);
-	int exp;
+    if (array == NULL || size < 2)
+        return;
 
-	for (exp = 1; max / exp > 0; exp *= 10)
-	{
-		countingSort(array, size, exp);
-		print_array(array, size);
-	}
+    int max_value = array[0];
+    for (size_t i = 1; i < size; i++) {
+        if (array[i] > max_value)
+            max_value = array[i];
+    }
+
+    int exp = 1;
+    int *output = malloc(size * sizeof(int));
+    if (output == NULL)
+        return;
+
+    while (max_value / exp > 0) {
+        int count[10] = {0};
+
+        for (size_t i = 0; i < size; i++)
+            count[(array[i] / exp) % 10]++;
+
+        for (int i = 1; i < 10; i++)
+            count[i] += count[i - 1];
+
+        for (int i = size - 1; i >= 0; i--)
+            output[--count[(array[i] / exp) % 10]] = array[i];
+
+        for (size_t i = 0; i < size; i++)
+            array[i] = output[i];
+
+        print_array(array, size);
+        exp *= 10;
+    }
+
+    free(output);
 }
