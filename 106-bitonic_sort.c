aefeb55f5ef86872fe322e22d0cc73373e125d1d
Reliccode <bryanmaritim@gmail.com>
2023-07-28 15:40:27 +0000

    Avoiding stack smaching and incorrect results

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -39,31 +39,7 @@ void bitonic_merge(int *array, size_t low, size_t count, int up)
 }
 
 /**
- * bitonic_merge_down - Merges two subarrays in descending order
- * @array: Pointer to the array
- * @low: Starting index of the first subarray
- * @count: Number of elements to be merged
- */
-void bitonic_merge_down(int *array, size_t low, size_t count)
-{
-	if (count > 1)
-	{
-		size_t k = count / 2;
-		size_t i;
-
-		for (i = low; i < low + k; i++)
-		{
-			if (array[i] < array[i + k])
-				swap(&array[i], &array[i + k]);
-		}
-
-		bitonic_merge_down(array, low, k);
-		bitonic_merge_down(array, low + k, k);
-	}
-}
-
-/**
- * bitonic_sort_recursive_up - func for bitonic sort in ascending order
+ * bitonic_sort_recursive_up - Recursive function for bitonic sort in ascending order
  * @array: Pointer to the array to be sorted
  * @low: Starting index of the range to be sorted
  * @count: Number of elements to be sorted
@@ -74,13 +50,13 @@ void bitonic_sort_recursive_up(int *array, size_t low, size_t count)
 	{
 		size_t k = count / 2;
 
-		bitonic_sort_recursive_up(array, low, k);
-		bitonic_sort_recursive_up(array, low + k, k);
-
 		printf("Merging [%lu/%lu] (UP):\n", count, count * 2);
 		print_array(array + low, count);
 
-		bitonic_merge_down(array, low, count);
+		bitonic_sort_recursive_up(array, low, k);
+		bitonic_sort_recursive_up(array, low + k, k);
+
+		bitonic_merge(array, low, count, 1);
 
 		printf("Result [%lu/%lu] (UP):\n", count, count * 2);
 		print_array(array + low, count);
@@ -88,7 +64,7 @@ void bitonic_sort_recursive_up(int *array, size_t low, size_t count)
 }
 
 /**
- * bitonic_sort - Sorts array of ints in ascending order using Bitonic sort
+ * bitonic_sort - Sorts an array of integers in ascending order using Bitonic sort
  * @array: Pointer to the array to be sorted
  * @size: Number of elements in the array
  */
